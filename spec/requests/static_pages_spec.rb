9698bc2658422ad6f9b6d7bbfb37754c0556032e
Scott <burnssa@gmail.com>
2014-06-06 20:17:25 -0700

    Finished layout and signup page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,45 +1,68 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
 
-		it "should have the title 'Home'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
-    end
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  subject { page }
+
+  shared_examples_for "all static pages" do
+
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+
+    before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home')}
   end
 
   describe "Help page" do
-    it "should have the content 'Help page'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
 
-    it "should have the title 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      expect(page).to have_title('Help')
-    end
+    before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
    describe "About page" do
-    it "should have the content 'About Us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
 
-   it "should have the title 'About Us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      expect(page).to have_title('About Us')
-    end
+    before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Contact page" do
+   
+    before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
 	end
+
+    it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
+
+
 end
